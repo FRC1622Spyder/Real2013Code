cb1353f6cf86f637292823d9461bd1b6af757b7f
Ian <netbug@cox.net>
2013-02-23 15:42:43 -0800

    missed some important things

--- Pickup.cpp
+++ Pickup.cpp
@@ -14,8 +14,8 @@ class Pickup : public Spyder::Subsystem
 	Spyder::ConfigVar<UINT32> arm;
 	Spyder::TwoIntConfig stick;
 public:
-	Pickup() : Spyder::Subsystem("Pickup"), ExtSol("close_gripper_sol",5), RetSol("open_gripper_sol", 6),
-	climbExt("bind_close_gripper", 3, 6), climbRet("bind_open_gripper", 3, 7), //FIXME: get appropreate button numbers
+	Pickup() : Spyder::Subsystem("Pickup"), ClampSol("close_gripper_sol",5), ReleaseSol("open_gripper_sol", 6),
+	cSol("bind_close_gripper", 3, 6), rSol("bind_open_gripper", 3, 7), //FIXME: get appropreate button numbers
 	arm("pickup_arm_mot", 12), stick("bind_pickup_arm_axis", 3, 2)
 	{
 		
@@ -45,11 +45,11 @@ public:
 			
 			Spyder::GetVictor(arm.GetVal())->Set(Spyder::GetJoystick(stick.GetVar(1))->GetRawAxis(stick.GetVar(2)));
 						
-			if(Spyder::GetJoystick(climbExt.GetVar(1))->GetRawButton(climbExt.GetVar(2))==true) {
+			if(Spyder::GetJoystick(cSol.GetVar(1))->GetRawButton(cSol.GetVar(2))==true) {
 				Spyder::GetSolenoid(ClampSol.GetVal())->Set(true);
 				Spyder::GetSolenoid(ReleaseSol.GetVal())->Set(false);
 			}
-			else if(Spyder::GetJoystick(climbRet.GetVar(1))->GetRawButton(climbRet.GetVar(2))==true) {
+			else if(Spyder::GetJoystick(rSol.GetVar(1))->GetRawButton(rSol.GetVar(2))==true) {
 				Spyder::GetSolenoid(ClampSol.GetVal())->Set(false);
 				Spyder::GetSolenoid(ReleaseSol.GetVal())->Set(true);
 			}
@@ -66,4 +66,4 @@ public:
 	}
 };
 
-Climb climb;
+Pickup pickup;
