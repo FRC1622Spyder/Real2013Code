8ccc5bb444a9dea6f12ad0287c2591a96c27e6c0
Regional Driver Station <N/A>
2013-03-08 11:55:32 -0800

    New idea for console, removed old added skeleton for new and implemented Spyder::Packet

--- Console.h
+++ Console.h
@@ -1,14 +1,42 @@
 #pragma once
-#include "Subsystem.h"
+#include <string>
 
-class Console : public Spyder::Subsystem
+namespace Spyder
 {
-	private:
-		Console();
-	public:
-		virtual void Init(Spyder::RunModes runmode);
+	class Packet
+	{
+		private:
+			std::string strData;
+		public:
+			Packet() : strData()
+			{
+			}
 		
-		virtual void Periodic(Spyder::RunModes runmode);
-		
-		virtual void RobotInit();
+			template<class T>
+			void AddData(T t)
+			{
+				strData += std::string((char*)(&t), sizeof(T));
+			}
+			
+			void AddData(char* t)
+			{
+				unsigned int len = strlen(t);
+				strData += std::string((char*)(&len), sizeof(unsigned int));
+				strData += t;
+			}
+			
+			void AddData(char *t, unsigned int length)
+			{
+				strData += std::string(t, length);
+			}
+	};
+	
+	class Console
+	{
+		public:
+			Console();
+			~Console();
+			bool Connect(std::string strIP, unsigned short usPort);
+			bool SendData(std::string strData);
+	};
 };
