3e144f8911e0e34bf2e823ff3c6bf9e9f1479042
Regional Driver Station <N/A>
2013-03-29 08:58:45 -0700

    Progress, fuck you wpilib

--- Turret.cpp
+++ Turret.cpp
@@ -5,6 +5,45 @@
 #include <math.h>
 #include <iostream>
 
+class RollAvg
+{
+	private:
+		std::vector<double> m_vals;
+		size_t pos;
+		size_t m_max;
+	public:
+		RollAvg(unsigned int n) : m_vals(n), pos(0), m_max(0)
+		{
+		}
+		
+		double Get()
+		{
+			double val = 0;
+			for(size_t i = 0; i < m_max; ++i)
+			{
+				val += m_vals[i];
+			}
+			if(m_max == 0)
+				return 0;
+			return val/double(m_max);
+		}
+		
+		void Add(double val)
+		{
+			size_t absMax = m_vals.size();
+			if(pos > absMax)
+			{
+				pos = 0;
+				m_max = absMax;
+			}
+			else if(m_max < absMax)
+			{
+				m_max = pos;
+			}
+			m_vals[pos++] = val;
+		}
+};
+
 class Turret : public Spyder::Subsystem, public PIDOutput, public PIDSource
 {
 	private:
@@ -41,7 +80,7 @@ class Turret : public Spyder::Subsystem, public PIDOutput, public PIDSource
 		timespec lastRead;
 		PIDController pid;
 		
-		double rpm;
+		double m_rpm;
 	public:
 		Turret() : Spyder::Subsystem("Turret"), frontMotor("frontTurretMotor", 4),
 			backMotor("backTurretMotor", 3), turretJoystick("bind_turretSpeed", 3, 1),
@@ -52,13 +91,14 @@ class Turret : public Spyder::Subsystem, public PIDOutput, public PIDSource
 			pistonSolenoidRet("turret_pistonSolenoidRet", 2),
 			fireButton("bind_turretFire", 3, 2), stopButton("bind_turretStop", 3, 11),
 			presetButton("bind_turretPreset", 3, 1), wheel1cntr(4), wheel2cntr(5),
-			pid(0, 0, 0, this, this), rpm(0)
+			pid(0, 0, 0, 0.3f, this, this, 0.05), m_rpm(0)
 		{
 			lastRead.tv_nsec = 0;
 			lastRead.tv_sec = 0;
 			wheel1cntr.Start();
 			wheel2cntr.Start();
 			pid.Disable();
+			pid.SetOutputRange(-1, 1);
 		}
 		
 		virtual ~Turret()
@@ -67,23 +107,37 @@ class Turret : public Spyder::Subsystem, public PIDOutput, public PIDSource
 		
 		virtual void PIDWrite(float output)
 		{
-			speed += output;
+			/*speed += output*0.01;
+			speed = output;
+			if(speed > 1.f)
+			{
+				speed = 1.f;
+			}*/
 		}
 		
 		virtual double PIDGet()
 		{
+			static RollAvg avg(10);
+			timespec read;
 			float time1 = lastRead.tv_sec;
 			time1 += float(lastRead.tv_nsec)*1e-9;
-			clock_gettime(CLOCK_REALTIME, &lastRead);
-			float time2 = lastRead.tv_sec;
-			time2 += float(lastRead.tv_nsec)*1e-9;
+			clock_gettime(CLOCK_REALTIME, &read);
+			float time2 = read.tv_sec;
+			time2 += float(read.tv_nsec)*1e-9;
 			float time = time2-time1;
-			int revs = wheel1cntr.Get();
+			if(time < 0.09f)
+			{
+				return m_rpm;
+			}
+			clock_gettime(CLOCK_REALTIME, &lastRead);
+			double revs = wheel1cntr.Get()/3.0;
 			wheel1cntr.Reset();
-			double rpm = revs;
-			rpm *= time;
-			rpm /= 60.f;
-			return rpm;
+			double derp = 60.0/time;
+			double my_rpm = revs;
+			my_rpm *= derp;
+			avg.Add(my_rpm);
+			m_rpm = avg.Get();
+			return m_rpm;
 		}
 		
 		virtual void Init(Spyder::RunModes runmode)
@@ -105,7 +159,6 @@ class Turret : public Spyder::Subsystem, public PIDOutput, public PIDSource
 		
 		virtual void Periodic(Spyder::RunModes runmode)
 		{
-			pid.SetSetpoint(rpm);
 			switch(runmode)
 			{
 				case Spyder::M_TELEOP:
@@ -116,18 +169,25 @@ class Turret : public Spyder::Subsystem, public PIDOutput, public PIDSource
 					float val = joystick->GetRawAxis(turretJoystick.GetVar(2)) * -1;
 					val = fabs(val) > Spyder::GetDeadzone() ? val : 0;
 					//val *= inputMul.GetVal();
-					rpm = Spyder::GetJoystick(1)->GetRawAxis(3);
+					double rpm = Spyder::GetJoystick(1)->GetRawAxis(3);
 					rpm += 1;
 					rpm /= 2.0;
+					rpm *= 5000;
+					pid.SetSetpoint(rpm);
 					double p = Spyder::GetJoystick(2)->GetRawAxis(3);
 					p += 1;
 					p /= 2.0;
-					pid.SetPID(p, 0, 0);
+					p *= 0.001f;
+					pid.SetPID(0.0005f, 0, 0);
+					pid.Calculate();
+					speed = p * pid.GetError();
+					speed += 0.3;
 					//speed += val;
 					//speed = (speed > 1.f) ? 1.f : speed;
-					if(val != 0.0f)
+					//if(val != 0.0f)
 					{
-						std::cout << "New turret speed: " << speed << ", Setpoint: " << rpm << ", P: " << p << std::endl;
+						std::cout << "New turret speed: " << speed << ", Setpoint: " << rpm << ", P: " << p
+							<< "RPM: " << m_rpm << "Error: " << pid.GetError() << std::endl;
 						Spyder::Packet p;
 						p.AddData(speed);
 						Spyder::Console::GetSingleton()->SendPacket("turr7et", p);	
@@ -143,10 +203,10 @@ class Turret : public Spyder::Subsystem, public PIDOutput, public PIDSource
 						speed = 0;
 					}
 					
-					if(speed < 0)
+					/*if(speed < 0.f)
 					{
 						speed = 0;
-					}
+					}*/
 					
 					if(frontInv.GetVal())
 					{
